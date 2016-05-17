26c840157276edc896d70c3e4232a5d1016fe92e
Joakim Ahrlin <joakim.ahrlin@gmail.com>
2016-05-17 20:00:39 +0200

    release 2

--- _js/main.js
+++ _js/main.js
@@ -6,3 +6,16 @@ var feed = new Instafeed({
     template: '<div class="col-xs-12 col-md-4 instafeed__item"><a class="instafeed__image" href="{{link}}"><img src="{{image}}" /><div class="instafeed__item__meta"><span class="instafeed__item__likes"><i class="fa fa-heart" aria-hidden="true"></i>{{likes}}</span><span class="instafeed__item__comments"><i class="fa fa-comment" aria-hidden="true"></i>{{comments}}</span></div></a></div>'
 });
 feed.run();
+
+$(document).ready(function() {
+    $('nav a[href="#menu"]').on('click', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        $('body').addClass('js-menu-toggled');
+    });
+
+    $('nav .close').on('click', function(event) {
+        event.preventDefault();
+        $('body').removeClass('js-menu-toggled');
+    });
+});
