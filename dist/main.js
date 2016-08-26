6bc9d0502cfc76c9ee23e64fbc45d4efa544ff09
Joakim Ahrlin <joakim.ahrlin@gmail.com>
2016-08-26 09:25:55 +0200

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -3219,7 +3219,7 @@
 var feed = new Instafeed({
     get: "user",
     userId: 253231379,
-    accessToken: "461112356.1677ed0.ea238e84513f400b9f9ad2cb8abfd597",
+    accessToken: "253231379.d8d1d50.12cd71c254a4460ebfc758f1e7b87778",
     resolution: "low_resolution",
     template: '<div class="col-xs-12 col-md-4 instafeed__item"><a class="instafeed__image" href="{{link}}"><img src="{{image}}" /><div class="instafeed__item__meta"><span class="instafeed__item__likes"><i class="fa fa-heart" aria-hidden="true"></i>{{likes}}</span><span class="instafeed__item__comments"><i class="fa fa-comment" aria-hidden="true"></i>{{comments}}</span></div></a></div>'
 });
